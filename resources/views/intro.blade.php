d7a00d78ffa1715c6cebd5766dd7b551b84e2d61
Julio Cesar Coto Flores <julioccflores@outlook.com>
2024-08-21 14:35:23 -0600

    Delete resources/views directory

--- resources/views/intro.blade.php
+++
@@ -1,8 +0,0 @@
-
-@section('content')
-<div class="welcome-message">
-    <h1>Bienvenido al Sistema de Gestión</h1>
-    <p>Estamos contentos de tenerte aquí. Utiliza el menú superior para navegar a través de las diferentes secciones del sistema.</p>
-</div>
-@endsection
-
